27529158b600efe4993294a62a95a8a22fee235c
Vanessa Aristizabal <angie_van.fajardo@uao.edu.co>
2022-11-10 16:46:48 -0500

    Sonido bg

--- Assets/Systems/CinematicSystemSimple.cs
+++ Assets/Systems/CinematicSystemSimple.cs
@@ -11,24 +11,35 @@ public class CinematicSystemSimple : MonoBehaviour
     private VideoPlayer player;
     private GameObject next;
     public string nextRawImage = "";
+    public AudioSource audioS;
 
     
     void Start()
     {
         player = GetComponent<VideoPlayer>();
         next = GameObject.Find(nextRawImage);
+        audioS = GetComponent<AudioSource>();
     }
 
     void Update()
     {
+        Cinem();
+    }
+
+    void Cinem()
+    {
         if (player.time >= Math.Floor(player.length))
         {
             player.Stop();
+            
             GetComponent<RawImage>().enabled = false;
-            if(next != null)
+            if (next != null)
             {
+                audioS.Stop();
                 next.GetComponent<VideoPlayer>().Play();
             }
         }
+       
     }
+
 }
